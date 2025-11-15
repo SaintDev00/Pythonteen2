098160900027735c090c396d9566c34425bf8438
Saint Dev <jqydjdj@gmail.com>
2025-11-14 21:44:39 -0500

    Update One.py

--- One.py
+++ One.py
@@ -1,79 +1,45 @@
-while True: 
-    try: 
-        edad = int(input("Tell me how old are you"))
-        
-        while True:
-            Trabaja = input("Tell me if your work")
-            if edad>60 and Trabaja == "no":
-                print("You are a old man")
-                break
-            
-            elif edad>60 and Trabaja == "si":
-                print("No determinado")
-                break
-            else:
-                print ("Incorrecto")
-              
-        
-        if edad>25 and Trabaja == "no":
-            print("No determinado") 
-            break
-        
-        elif edad >25 and Trabaja == "si":
-            print("You are a active adult")
-            break
+edad=int(input("Cual es tu edad?"))
+
+if edad < 0 or edad >120:
+    print("Edad no valida")
+
+else:
+    if edad < 6:
+        print("Infante")
+
+
+
+    elif edad >= 6 and edad <=17:
+        estudia=input("¿Estudia?si/no:").lower()
+        if estudia == "si":
+            print("estudiante escolar")
         else:
-              print("Incorrecto")
-        
-        while True:
-        
-            if edad >18 and edad<=25:
-                N_escolar=input("You studied? yes/no : ").lower ()
-                
-                if N_escolar == "yes":
-                        print("You are a university student")  
-                        break
-                
-                elif N_escolar == "no":
-                        print("You arent university student")
-                        break
-                else:
-                      print("Wrong answer..choose yes or no")
-                      
-        while True:
-              
-                if edad >6 and edad<=17:
-                        N_escolar=input("You studied? yes/no : ").lower()
+            print("No determinado")
 
-                if N_escolar == "yes":
-                        print ("tu eres un estudiante escolar")
-                        break
-                
-                elif N_escolar == "no":
-                        print ("tu no eres un estudiante escolar")
-                        break
-                else:
-                      print("Wrong answer..choose yes or no") 
+    elif edad >= 18 and edad <= 25:
+        estudia=input("¿Estudia en la universidad? si/no").lower()
+        if estudia == "si":
+            print("Estudiante universitario")
+        else:
+            print("No determinado")
 
-        while True:
-              
-                if edad <6:
-                        print("You are a kid")
-                else:
-                    print("You need have more years of six")
+    elif edad > 60:
+        jubilado=input("trabajas?si/no").lower()
+        if jubilado == "no":
+            print("Eres un adulto mayor y jubilado")
+        else:
+            print("No determinado")
 
-    except ValueError:
-          print("Try with a  whole number")
-            
-      
-            
 
-            
+    elif edad > 25:
+        Activo=input("Trabajas? si/no").lower()
+        if Activo == "si":
+            print("Eres un adulto activo")
+        else:
+            print("No determinado")
     
-            
-
-
-
+    else:
+        print("No determinado")
 
 
    
